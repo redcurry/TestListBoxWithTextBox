3cae9681b87f24cfa0fa6a7dee827c74ad5bfb66
Carlos Anderson <canderson@radformation.com>
2019-11-13 11:10:33 -0500

    Add initial solution

---
+++ TestListBoxWithTextBox/ItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace TestListBoxWithTextBox
+{
+    public class ItemViewModel : ViewModelBase
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => Set(nameof(Name), ref name, value);
+        }
+    }
+}
---
+++ TestListBoxWithTextBox/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace TestListBoxWithTextBox
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<ItemViewModel> Items { get; } =
+            new ObservableCollection<ItemViewModel>();
+
+        private ItemViewModel selectedItem;
+        public ItemViewModel SelectedItem
+        {
+            get => selectedItem;
+            set => Set(nameof(SelectedItem), ref selectedItem, value);
+        }
+
+        public ICommand AddItemCommand =>
+            new RelayCommand(() =>
+            {
+                var newItem = new ItemViewModel();
+                Items.Add(newItem);
+                SelectedItem = newItem;
+            });
+    }
+}
